9c9acd5486dbf797c7c9e36b7219f2d6cc27d872
Yuqing Wu <ywu20@stuy.edu>
2021-12-10 18:03:50 -0500

    buy and sell stocks updating share values and adding / removing from database

--- app/stock.py
+++ app/stock.py
@@ -13,7 +13,7 @@ def create_db():
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
 
-    c.execute("CREATE TABLE IF NOT EXISTS stock_info (username TEXT, stock TEXT, shares INTEGER);")
+    c.execute("CREATE TABLE IF NOT EXISTS stock_info (user TEXT, stock TEXT, shares INTEGER);")
     db.commit()
     db.close()
 
@@ -24,14 +24,39 @@ def buy_sell(user, stock, amount):
                                     amount to buy: + for an actual buy action, - for a sell action
                                     user who's buying/selling the stock
         returns (boolean): true for success, false for fail
+    This function does not yet safe guard against the situation where stock shares may be negative
+    but the flask and html should make the user impossible to sell anything they don't have
     """
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
 
-    # if stock buying does not exist, add to database
-    c.execute("INSERT INTO stock_info VALUES(?,?,?)", (user, stock, amount))
+    # dict for passing into sql
+    dict = {"user":user, "stock":stock, "shares":0}
+
+    # get user's stocks
+    c.execute("SELECT stock FROM stock_info WHERE user = :user AND stock = :stock", dict)
+    exist = c.fetchall()
+    print(exist)
+
+    if (exist == []):
+        # if stock buying does not exist, add to database
+        c.execute("INSERT INTO stock_info VALUES(?,?,?)", (user, stock, amount))
+        print("Stock "+stock+ " added.")
+
+    else:
+        # if exist add amount
+        # get existing shares
+        c.execute("SELECT shares FROM stock_info WHERE user = :user AND stock = :stock", dict)
+        shares = c.fetchall()
+        print(shares)
+        dict["shares"] = shares[0][0] + amount
+        c.execute("UPDATE stock_info SET shares = :shares WHERE stock = :stock AND user = :user", dict)
     # if stock selling is 0, delete from database
-    # if exist add amount
+    c.execute("SELECT shares FROM stock_info WHERE user = :user AND stock = :stock", dict)
+    shares = c.fetchall()
+    if (shares[0][0] <= 0):
+        c.execute("DELETE FROM stock_info WHERE user=:user AND stock = :stock", dict)
+        print("stock "+stock+" deleted from database because all shares are sold")
     # update_cash for user
     # Yuqing will do
     db.commit()
@@ -61,4 +86,6 @@ def get_stock(user):
 
 # for testing ########
 create_db()
-buy_sell("user1", "APPL", 10)
+buy_sell("user1", "AAPL", 10)
+buy_sell("user1", "GOOG", 10)
+buy_sell("user1", "AMZN", 10)
